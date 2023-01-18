779fa3d96f8af2ef947ea6ec308b315dc5a334d8
sipuna.biswal <sipuna.biswal@gmail.com>
2023-01-18 12:07:32 +0530

    Sample codes

--- basics/hello.py
+++ basics/hello.py
@@ -1,4 +1,4 @@
 #print("Hellow world")
 #input("What is your name: ")
 
-print("Hello " + input("What is your name: "))
+print(len( input("What is your name: ")))
---
+++ basics/swap.py
@@ -0,0 +1,8 @@
+# Swap numbers using a variable
+a = input("a: ")
+b = input("b: ")
+c = a
+a = b
+b = c
+print("a: " + a)
+print("b: " + b)
